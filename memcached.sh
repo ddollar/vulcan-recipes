d345434a1f06f72b609e59c5b79a787a09634315
David Dollar <ddollar@gmail.com>
2012-07-15 13:23:57 -0400

    more memcached script

--- memcached.sh
+++ memcached.sh
@@ -3,9 +3,19 @@
 root=$(pwd)
 prefix="$root/vendor/memcached"
 
+# build into $prefix
 cd memcached-*
 ./configure --prefix="${prefix}"
 make install
 
+# clean up source tree
 cd $root
 rm -rf memcached-*
+
+# write profile.d
+mkdir -p $root/profile.d
+echo >$root/profile.d/memcached.sh <<EOF
+INCLUDE_PATH="/app/vendor/memcached/include:${INCLUDE_PATH}"
+LD_LIBRARY_PATH="/app/vendor/memcached/lib:${LD_LIBRARY_PATH}"
+PATH="/app/vendor/memcached/bin:${PATH}"
+EOF
